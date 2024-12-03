edfebbe83cc078884d2fb53fed623ce2401a77f0
srk2411 <srkbhagavatula@gmail.com>
2024-12-03 16:29:26 -0500

    Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,6 @@
-<script>
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-</script>
-
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
